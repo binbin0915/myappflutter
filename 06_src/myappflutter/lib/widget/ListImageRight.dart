d513e25dffb66a0887d7b2c216002d66c0366493
binbin0915 <184821967@qq.com>
2019-10-02 06:59:02 +0800

    首个代码版本提交

---
+++ 06_src/myappflutter/lib/widget/ListImageRight.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:myappflutter/model/model.dart';
+
+import 'BaseModuleWidget.dart';
+
+Widget ListImageRight(
+    BuildContext context, Feed feed) {
+  Widget widget;
+  widget = new Row(
+        children: <Widget>[
+          Container(
+            width: 200,
+            height: 120,
+            margin: EdgeInsets.all(0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: TitleLabel(context, feed.post.title),
+                  height: 90,
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.topLeft,
+                ),
+                Expanded(
+                  child: Container(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                      alignment: Alignment.bottomLeft,
+                      child: ListBottomWidget(context, feed.post)
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child:  Container(
+              margin: EdgeInsets.only(left: 0),
+//              width: 170,
+              height: 120,
+              child: Image.network(feed.image,fit: BoxFit.fitHeight,),
+            ),
+          ),
+        ],
+      );
+  return widget;
+}
